bf25e9f52720297d08b8db18c2c0cb1de3103df9
navik03 <61587450+navik03@users.noreply.github.com>
2020-03-28 11:43:38 +0530

    Add files via upload

---
+++ Websitelogin.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+usernameStr = '17BCS4023'
+passwordStr = 'N@vikR@na123'
+
+browser = webdriver.Chrome()
+browser.get(('https://uims.cuchd.in/uims/'))
+
+# fill in username and hit the next button
+
+username = browser.find_element_by_id('txtUserId')
+username.send_keys(usernameStr)
+
+nextButton = browser.find_element_by_id('btnNext')
+nextButton.click()
+
+# wait for transition then continue to fill items
+
+password = WebDriverWait(browser, 10).until(
+    EC.presence_of_element_located((By.NAME, "txtLoginPassword")))
+
+password.send_keys(passwordStr)
+
+signInButton = browser.find_element_by_id('btnLogin')
+signInButton.click()
